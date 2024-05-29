bcd819f84a68f097482fbcda2b616fc69d7078dc
Abdulkader A Abdi <69095293+aaabdulakader@users.noreply.github.com>
2024-05-29 13:44:37 -0500

    Fix error handling and ensure static files are served correctly

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/*", (req, res, next) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"), (err) => {
     if (err) {
-      res.status((500).send(err));
+      res.status(500).send(err);
     }
   });
 
