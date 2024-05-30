892ab52c58e425c17db7ab9f17864a6dfb73b64e
Abdulkader A Abdi <69095293+aaabdulakader@users.noreply.github.com>
2024-05-30 17:42:39 -0500

    Update CSP to allow external image sources

--- client/src/hooks/useIsLoggedIn.js
+++ client/src/hooks/useIsLoggedIn.js
@@ -3,7 +3,9 @@ const useLoggedIn = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    const token = document.cookie.includes("jwt");
+    const token = document.cookie.split(";").find((cookie) => {
+      return cookie.includes("jwt");
+    });
     if (token) {
       setLoggedIn(true);
     }
