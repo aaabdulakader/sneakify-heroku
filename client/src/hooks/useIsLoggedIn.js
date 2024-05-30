3e76a4dc50c573af7ac35ab6076b95399255c905
Abdulkader A Abdi <69095293+aaabdulakader@users.noreply.github.com>
2024-05-29 22:20:57 -0500

    Update CSP to allow external image sources

--- client/src/hooks/useIsLoggedIn.js
+++ client/src/hooks/useIsLoggedIn.js
@@ -1,21 +1,10 @@
 import { useState, useEffect } from "react";
-
-// Helper function to parse cookies
-const getCookie = (name) => {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(";").shift();
-  return null;
-};
-
 const useLoggedIn = () => {
   const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    const token = getCookie("jwt");
-    console.log("token", token);
+    const token = document.cookie.includes("jwt");
     if (token) {
-      // Optionally, validate the token with your backend here
       setLoggedIn(true);
     }
   }, []);
